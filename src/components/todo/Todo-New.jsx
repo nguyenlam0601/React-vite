a15c27921578ce381b853e571359608e9550d4b8
nguyenlam0601 <nguyenvanlam0601@gmail.com>
2025-04-02 16:36:39 +0700

    video36

--- src/components/todo/Todo-New.jsx
+++ src/components/todo/Todo-New.jsx
@@ -1,10 +1,22 @@
 const TodoNew = (props) => {
   const { myFucntion } = props;
+  const handleClick = () => {
+    alert("click me");
+  };
+  const handleOnchage = (name) => {
+    console.log(name);
+  };
   myFucntion("Ray");
   return (
     <div className="Todo-header">
-      <input type="text" placeholder="Enter your task"></input>
-      <button> Add</button>
+      <input
+        type="text"
+        placeholder="Enter your task"
+        onChange={(event) => {
+          handleOnchage(event.target.value);
+        }}
+      ></input>
+      <button onClick={handleClick}> Add</button>
     </div>
   );
 };
