ed9c740dde165ed665f6fa1d2f90f1fff2be1ecd
nguyenlam0601 <nguyenvanlam0601@gmail.com>
2025-04-02 14:23:12 +0700

    video34

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import TodoData from "./components/todo/Todo-Data";
 import TodoNew from "./components/todo/Todo-New";
-import "./components/todo/todo.css"
-import reactLogo from './assets/react.svg'
+import "./components/todo/todo.css";
+import reactLogo from "./assets/react.svg";
 const App = () => {
+  const name = "Eric";
+  const data = { age: 24, address: "HY" };
   return (
     <>
       <div className="container">
         <div className="Todo-name">Todo List</div>
         <TodoNew />
-        <TodoData />
+        <TodoData name={name} data={data} />
         <div className="todo-logo">
           <img src={reactLogo} alt="" className="logo" />
         </div>
--- src/components/todo/Todo-Data.jsx
+++ src/components/todo/Todo-Data.jsx
@@ -1,15 +1,16 @@
-const TodoData = () => {
+const TodoData = (props) => {
+  const { name, data } = props;
   return (
     <div className="Todo-body">
       <div className="Todo-data">
-        <div className="data-content">Do homework</div>
+        <div className="data-content">Do homework {name}</div>
         <div className="data-btn">
           <button>Delete</button>
           <button>Update</button>
         </div>
       </div>
       <div className="Todo-data">
-        <div className="data-content">Learn React</div>
+        <div className="data-content">My age is {data.age}</div>
         <div className="data-btn">
           <button>Delete</button>
           <button>Update</button>
@@ -17,5 +18,5 @@ const TodoData = () => {
       </div>
     </div>
   );
-}
-export default TodoData
+};
+export default TodoData;
