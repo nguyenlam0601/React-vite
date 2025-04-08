33d6930bcfe0433319c221d38e538fbcb1bfecb9
nguyenlam0601 <nguyenvanlam0601@gmail.com>
2025-04-08 17:41:44 +0700

    updateTodo

--- src/components/todo/Todo-Data.jsx
+++ src/components/todo/Todo-Data.jsx
@@ -2,20 +2,30 @@ import { useState } from "react";
 
 const TodoData = (props) => {
   const { todoList, deleteData } = props;
+  const [updateFlg, setUpdateFlg] = useState(false);
   const getId = (id) => {
     deleteData(id);
   };
-
+  const updateItem = (index) => {
+    setUpdateFlg(true);
+    alert(index);
+  };
   return (
     <div className="Todo-body">
-      {todoList.map((item) => {
+      {todoList.map((item, index) => {
         console.log("value", item.name);
         return (
           <div className="Todo-data" key={item.id}>
-            <div className="data-content">{item.name}</div>
+            {
+              (updateFlg = false ? (
+                <input type="text" />
+              ) : (
+                <div className="data-content">{item.name}</div>
+              ))
+            }
             <div className="data-btn">
               <button onClick={() => getId(item.id)}>Delete</button>
-              <button>Update</button>
+              <button onClick={() => updateItem(index)}>Update</button>
             </div>
           </div>
         );
