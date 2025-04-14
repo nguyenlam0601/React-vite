75bb2947cc47ad143346696c5556fade196f256c
nguyenlam0601 <nguyenvanlam0601@gmail.com>
2025-04-14 07:31:56 +0700

    axios

---
+++ src/components/user/user.form.jsx
@@ -0,0 +1,68 @@
+import { Button, Input } from "antd";
+import { useState } from "react";
+import axios from "axios";
+
+const UserForm = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassWord] = useState("");
+  const [phone, setPhone] = useState("");
+  const handleClick = () => {
+    const URL_BACKEND = "http://localhost:8080/api/v1/user";
+    const data = {
+      fullName,
+      email,
+      password,
+      phone,
+    };
+    axios.post(URL_BACKEND, data);
+  };
+  return (
+    <div className="user-form" style={{ margin: "20px 0" }}>
+      <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
+        <div>
+          <span>Full Name</span>
+          <Input
+            value={fullName}
+            onChange={(event) => {
+              setFullName(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <span>Email</span>
+          <Input
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <span>password</span>
+          <Input.Password
+            value={password}
+            onChange={(event) => {
+              setPassWord(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <span>Phone number</span>
+          <Input
+            value={phone}
+            onChange={(event) => {
+              setPhone(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <Button type="primary" onClick={handleClick}>
+            Create User
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default UserForm;
--- src/pages/userPages.jsx
+++ src/pages/userPages.jsx
@@ -1,4 +1,14 @@
+import UserForm from "../components/user/user.form";
+import UserTable from "../components/user/user.table";
+
 const userPages = () => {
-  return <div>User Pages</div>;
+  return (
+    <div>
+      <div style={{ padding: "20px" }}>
+        <UserForm />
+        <UserTable />
+      </div>
+    </div>
+  );
 };
 export default userPages;
