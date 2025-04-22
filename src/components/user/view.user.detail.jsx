690f22b0e16d4a5087e9221c1ab015e4f298488a
nguyenlam0601 <nguyenvanlam0601@gmail.com>
2025-04-23 00:05:06 +0700

    view avatar

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -9,7 +9,12 @@ const ViewUserDetail = (props) => {
   if (!dataDetail) return null;
   return (
     <>
-      <Drawer title="View Detail" onClose={onClose} open={isOpenDetail}>
+      <Drawer
+        width={"40vw"}
+        title="View Detail"
+        onClose={onClose}
+        open={isOpenDetail}
+      >
         <p>Id: {dataDetail._id}</p>
         <br />
         <p>Full Name: {dataDetail.fullName}</p>
@@ -18,6 +23,37 @@ const ViewUserDetail = (props) => {
         <br />
         <p>Phone: {dataDetail.phone}</p>
         <br />
+        <div
+          style={{ width: "150px", height: "100px", border: "1px solid #ccc" }}
+        >
+          <img
+            style={{ width: "100%", height: "100%", objectFit: "contain" }}
+            src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${
+              dataDetail.avatar
+            }`}
+            alt=""
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="btnUpload"
+            style={{
+              display: "inline-block",
+              padding: "10px 20px",
+              marginTop: "20px",
+              background: "#4CAF50",
+              color: "white",
+              fontSize: "16px",
+              border: "none",
+              borderRadius: "8px",
+              textAlign: "center",
+              cursor: "pointer",
+            }}
+          >
+            Upload file
+          </label>
+          <input type="file" hidden id="btnUpload" />
+        </div>
       </Drawer>
     </>
   );
